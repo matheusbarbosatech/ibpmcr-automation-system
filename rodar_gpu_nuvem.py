fc65eed0d8d7de34818e9288118186dd91e25d3d
Matheus Barbosa <matheusbarbosa860@gmail.com>
2026-08-14 23:50:13 -0300

    fix(gpu): adicionar kernelspec python3 em notebook_data para suporte ao Papermill no Kaggle

--- rodar_gpu_nuvem.py
+++ rodar_gpu_nuvem.py
@@ -210,6 +210,11 @@ except Exception as fatal_err:
             }
         ],
         "metadata": {
+            "kernelspec": {
+                "display_name": "Python 3",
+                "language": "python",
+                "name": "python3"
+            },
             "language_info": {
                 "name": "python"
             }
